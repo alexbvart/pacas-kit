6c509513222ab24fcca3d65f89733e5a3b289e10
alfonso.briones <indra.abriones@cuida.pe>
2025-12-24 18:02:21 -0500

    Updates

---
+++ _app/immutable/chunks/DMzdHfAz.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1y3s6dw?.base??"/pacas-kit",a=globalThis.__sveltekit_1y3s6dw?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DcGmJ5Yb.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_17vrzqx?.base??"/pacas-kit",a=globalThis.__sveltekit_17vrzqx?.assets??s??"";export{a,s as b};
